d435ae6464d95a02d83bf46b412991704c447bac
lovedeepsh <lovedeeps789@gmail.com>
2018-06-08 16:41:31 +0530

    javain updated

--- softwareinstallfiles/javains.sh
+++ softwareinstallfiles/javains.sh
@@ -3,6 +3,6 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
 sudo apt-get install oracle-java8-set-default
-echo >> /etc/environment
-JAVA_HOME=/usr/lib/jvm/java-8-oracle
-JRE_HOME=/usr/lib/jvm/java-8-oracle/jre
+echo "JAVA_HOME=/usr/lib/jvm/java-8-oracle /n JRE_HOME=/usr/lib/jvm/java-8-oracle-jre" >> /etc/environment
+
+
